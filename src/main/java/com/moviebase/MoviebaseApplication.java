ef8b604d21b3d35497e356e86af617697952d149
vincent torrijos <vtorrijos18@live.com>
2019-05-09 22:54:05 -0500

    first movie-base-service commit

---
+++ src/main/java/com/moviebase/MoviebaseApplication.java
@@ -0,0 +1,13 @@
+package com.moviebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviebaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoviebaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moviebase/MoviebaseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.moviebase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MoviebaseApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
